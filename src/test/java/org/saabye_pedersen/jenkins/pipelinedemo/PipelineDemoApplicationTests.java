3ea72675bb3ba4b1c23994e8edfa6db261194192
Kim Saabye Pedersen <k.epost@gmail.com>
2017-12-05 09:29:28 +0100

    first

---
+++ src/test/java/org/saabye_pedersen/jenkins/pipelinedemo/PipelineDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package org.saabye_pedersen.jenkins.pipelinedemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PipelineDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
